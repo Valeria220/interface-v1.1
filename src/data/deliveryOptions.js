6392fa40763d82ae8b1255f295c19e5774eb6a18
Valeria220 <lera.kotegova@yandex.ru>
2025-10-17 11:03:16 +0500

    Уточнение вариантов получения заказа

--- src/data/deliveryOptions.js
+++ src/data/deliveryOptions.js
@@ -1,7 +1,5 @@
 //данные о вариантах доставки
 export const deliveryOptions = [
-  { id: "delivery", name: "С доставкой" },
-  { id: "pickup", name: "Самовывоз" },
   { id: "fast", name: "Моментальный заказ" },
   { id: "reservation", name: "Предзаказ" }
 ];
